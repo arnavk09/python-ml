6b27caec15465d2a6e5e9c1b42793ac853f9f8de
Arnav Katgeri <arnav02@outlook.com>
2022-01-27 23:19:29 +0530

    It Begins

---
+++ tuple_string_loop/tupStringLoop.py
@@ -0,0 +1,9 @@
+programmingLang = ('Python', 'Java', 'C++')
+for x in programmingLang:
+    print(x)
+
+print("I'm doing this for loop on my own : ")
+x=[1,2,3,4,5]
+for y in x:
+    # y=y+1
+    print(y*2)
